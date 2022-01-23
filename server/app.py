dedeb06ab2f1d4f14176b976fb55eb422211641d
Alexander Martin <alex@kugma.de>
2022-01-23 16:53:37 +0100

    add socket to server

--- server/app.py
+++ server/app.py
@@ -1,8 +1,11 @@
+from socket import socket
 from sys import int_info
 from dotenv import load_dotenv
 from os import path, getenv
 from distutils.log import debug
 from flask import Flask, request
+from flask_cors import CORS
+from flask_socketio import SocketIO, emit, send
 import pigpio
 
 load_dotenv(f"{path.dirname(__file__)}/.env")
@@ -40,6 +43,7 @@ def set_color(color):
         pi.set_PWM_dutycycle(PIN_B, blue)
 
     current_color = color
+    return current_color
 
 
 def led_off():
@@ -49,6 +53,7 @@ def led_off():
 
 
 def led_on():
+    global current_color
     if not current_color or current_color == "":
         set_color("#ffffff")
     else:
@@ -59,31 +64,44 @@ def led_on():
 # || WEBSERVER
 # ||================================================================================================
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "secret123"
+
+CORS(app)
+
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 
 @app.route("/desk_control/api/state", methods=["GET"])
 def state():
-    global last_color
     return {"color": current_color}
 
 
 @app.route("/desk_control/api/on", methods=["GET"])
 def on():
     led_on()
+    socketio.emit("color", current_color)
     return {"color": current_color}
 
 
 @app.route("/desk_control/api/off", methods=["GET"])
 def off():
     led_off()
+    socketio.emit("color", current_color)
     return {"color": current_color}
 
 
 @app.route("/desk_control/api/color", methods=["POST"])
 def color():
     set_color(request.form.get("color"))
+    socketio.emit("color", current_color)
     return {"color": current_color}
 
 
+@socketio.on("color")
+def on_color(data):
+    set_color(data)
+    emit("color", current_color, include_self=False, broadcast=True)
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True, port=5000)
+    socketio.run(app, host="0.0.0.0", debug=True, port=5000)
--- server/wsgi.py
+++ server/wsgi.py
@@ -1,6 +1,4 @@
 from app import app
-import led_controller
 
 if __name__ == "__main__":
-    led_controller.run()
     app.run()
