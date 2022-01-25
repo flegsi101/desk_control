2a6c7e633f1921b828af903170e6aa1c5bbd981d
Alexander Martin <alex@kugma.de>
2022-01-25 11:55:15 +0100

    client work

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,57 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import "./App.scss"
-import { Color, ColorChangeHandler, HuePicker } from "react-color"
+import { HexColorPicker } from "react-colorful"
 import axios from "axios"
+import socketIOClient, { Socket } from "socket.io-client"
+import Toggle from "./components/Toggle"
 
 const api = axios.create({
-  baseURL: "http://192.168.178.5/desk_control/api",
+  baseURL: "http://192.168.178.5:5000/desk_control/api",
 })
 
+const SOCKET = "http://192.168.178.5:5000"
+const socket = socketIOClient(SOCKET)
+
 function App() {
-  const [color, setColor] = useState<Color>()
+  const [color, setColor] = useState<string>()
+
+  useEffect(() => {
+    socket.on("color", (color) => {
+      setColor(color)
+    })
+  })
+
+  const changeColor = (color: string) => {
+    socket.emit("color", color)
+    setColor(color)
+  }
+
+  useEffect(() => {
+    if (!color) {
+      api.get("/state").then((res) => {
+        if (res.status == 200) {
+          setColor(res.data.color)
+        }
+      })
+    }
+  })
 
   return (
-    <div className="App">
-      <HuePicker color={color} onChange={(color) => setColor(color.hex)} />
-      <p>{color}</p>
+    <div className="app">
+      <div className="control led-toggle">
+        <p>Toggle led</p>
+        <Toggle></Toggle>
+      </div>
+      <div className="control led-toggle">
+        <div className="label">
+          <p>Select color</p>
+        </div>
+        <div className="led-color--preview">
+          <p>Selected: {color}</p>
+          <div style={{ background: color }}></div>
+        </div>
+        <HexColorPicker color={color} onChange={changeColor} />
+      </div>
     </div>
   )
 }
---
+++ client/src/components/Toggle.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import "./Toggle.scss"
+
+function Toggle() {
+  return (
+    <div className="toggle">
+      <div className="toggle--knob"></div>
+      <div className="toggle--body"></div>
+    </div>
+  )
+}
+
+export default Toggle
